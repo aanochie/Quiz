b446442d8ee45fdd3c7a3ebc31a87cb62c46a0b4
c2021423 <c2021423@ncl.ac.uk>
2023-03-08 15:21:15 +0000

    Added a basic interface to Main.java

--- src/main/java/csc1035/project2/Main.java
+++ src/main/java/csc1035/project2/Main.java
@@ -1,4 +1,29 @@
 package csc1035.project2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        boolean running = true;
+        while (running) {
+            Scanner myScanner = new Scanner(System.in);
+            System.out.println("What would you like to do?\n1. Create/Update/Delete a question.\n2. List questions.\n3. Create/Delete a quiz.\n4. Take a quiz.\n5. Exit");
+
+            String choice = myScanner.nextLine();
+
+            List<String> validChoices = Arrays.asList("1", "2", "3", "4", "5");
+
+            while (!validChoices.contains(choice)) {
+                System.out.println("Invalid choice. Please enter a number from 1-4:");
+                choice = myScanner.nextLine();
+            }
+
+            switch (choice) {
+                case "1" -> QuestionIO.main();
+                case "5" -> running = false;
+            }
+        }
+    }
 }
