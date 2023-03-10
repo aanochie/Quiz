3c895d8a1413b299da05973d8d61823d3de875c3
c2021423 <c2021423@ncl.ac.uk>
2023-03-10 11:58:15 +0000

    renamed column names in Quiz.java

--- src/main/java/csc1035/project2/Quiz.java
+++ src/main/java/csc1035/project2/Quiz.java
@@ -8,70 +8,70 @@ import javax.persistence.*;
 public class Quiz {
     @Id
     @GeneratedValue
-    @Column(name = "Quiz ID")
+    @Column(name = "id")
     private int id; // primary key
 
-    @Column(name = "Previous Score")
+    @Column(name = "score")
     private int score; // Users previous score of set quiz
 
-    @Column(name = "Question 1 ID")
+    @Column(name = "qid_1")
     private int qid_1; // question id 1 
 
-    @Column(name = "Question 2 ID")
+    @Column(name = "qid_2")
     private int qid_2; // question id 2 
 
-    @Column(name = "Question 3 ID")
+    @Column(name = "qid_3")
     private int qid_3; // question id 3 
 
-    @Column(name = "Question 4 ID")
+    @Column(name = "qid_4")
     private int qid_4; // question id 4 
 
-    @Column(name = "Question 5 ID")
+    @Column(name = "qid_5")
     private int qid_5; // question id 5 
 
-    @Column(name = "Question 6 ID")
+    @Column(name = "qid_6")
     private int qid_6; // question id 6 - "-1" if 5 question quiz
 
-    @Column(name = "Question 7 ID")
+    @Column(name = "qid_7")
     private int qid_7; // question id 7 - "-1" if 5 question quiz
 
-    @Column(name = "Question 8 ID")
+    @Column(name = "qid_8")
     private int qid_8; // question id 8 - "-1" if 5 question quiz
 
-    @Column(name = "Question 9 ID")
+    @Column(name = "qid_9")
     private int qid_9; // question id 9 - "-1" if 5 question quiz
 
-    @Column(name = "Question 10 ID")
+    @Column(name = "qid_10")
     private int qid_10; // question id 10 - "-1" if 5 question quiz
 
-    @Column(name = "Question 11 ID")
+    @Column(name = "qid_11")
     private int qid_11; // question id 11 - "-1" if 5/10 question quiz
 
-    @Column(name = "Question 12 ID")
+    @Column(name = "qid_12")
     private int qid_12; // question id 12 - "-1" if 5/10 question quiz
 
-    @Column(name = "Question 13 ID")
+    @Column(name = "qid_13")
     private int qid_13; // question id 13 - "-1" if 5/10 question quiz
 
-    @Column(name = "Question 14 ID")
+    @Column(name = "qid_14")
     private int qid_14; // question id 14 - "-1" if 5/10 question quiz
 
-    @Column(name = "Question 15 ID")
+    @Column(name = "qid_15")
     private int qid_15; // question id 15 - "-1" if 5/10 question quiz
 
-    @Column(name = "Question 16 ID")
+    @Column(name = "qid_16")
     private int qid_16; // question id 16 - "-1" if 5/10/15 question quiz
 
-    @Column(name = "Question 17 ID")
+    @Column(name = "qid_17")
     private int qid_17; // question id 17 - "-1" if 5/10/15 question quiz
 
-    @Column(name = "Question 18 ID")
+    @Column(name = "qid_18")
     private int qid_18; // question id 18 - "-1" if 5/10/15 question quiz
 
-    @Column(name = "Question 19 ID")
+    @Column(name = "qid_19")
     private int qid_19; // question id 19 - "-1" if 5/10/15 question quiz
 
-    @Column(name = "Question 20 ID")
+    @Column(name = "qid_20")
     private int qid_20; // question id 20 - "-1" if 5/10/15 question quiz
 
     public Quiz(int qid_1, int qid_2, int qid_3, int qid_4, int qid_5, int qid_6, int qid_7, int qid_8, int qid_9, int qid_10, int qid_11, int qid_12, int qid_13, int qid_14, int qid_15, int qid_16, int qid_17, int qid_18, int qid_19, int qid_20) {
@@ -95,6 +95,7 @@ public class Quiz {
         this.qid_18 = qid_18;
         this.qid_19 = qid_19;
         this.qid_20 = qid_20;
+        this.score = 0;
     }
 
     public Quiz() {
@@ -102,7 +103,7 @@ public class Quiz {
     }
 
     public static void main(String[] args) {
-        Quiz first_quiz = new Quiz(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16, 17, 18,19,20);
+        Quiz first_quiz = new Quiz(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1);
         first_quiz.save();
     }
 
