48a41ef030cea955e50dd4078bbf5bbbeddae5f2
nasimhannan <N.Hannan2@ncl.ac.uk>
2023-03-15 13:08:03 +0000

    Unversioned Commit

---
+++ src/main/java/csc1035/project2/TestNasim.java
@@ -0,0 +1,12 @@
+package csc1035.project2;
+import csc1035.project2.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import  java.util.Iterator;
+import  java.util.List;
+
+public class TestNasim {
+    public static void main(String[] args) {
+
+    }
+}
