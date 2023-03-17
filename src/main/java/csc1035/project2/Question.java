029ecb7fd11556f81e299e3feb807a2f12a3cbff
c2021423 <c2021423@ncl.ac.uk>
2023-03-17 11:23:26 +0000

    Updated all files to master

--- src/main/java/csc1035/project2/Question.java
+++ src/main/java/csc1035/project2/Question.java
@@ -1,6 +1,9 @@
 package csc1035.project2;
 
+import org.hibernate.Session;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "Questions")
@@ -36,6 +39,19 @@ public class Question {
     private int correct; // did the user get this question right the last time they attempted it
     // 1 if wrong last time, 2 if correct last time, 0 if never attempted
 
+    public String getTopic(){
+        return this.topic;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+
+    public int getCorrect(){return this.correct;}
+
+    public String getQuestion(){return this.question;}
+
+    public int getId(){return this.id;}
     // constructor for saq
     public Question(String question, String topic, int type, String answer) {
         this.question = question;
@@ -49,18 +65,44 @@ public class Question {
     }
 
     // constructor for mcq
-    public Question(String question, String topic, int type, String answer, String other1, String other2, String other3) {
+    public Question(String question, String topic, int type, String answer, ArrayList<String> otherAnswers) {
         this.question = question;
         this.topic = topic;
         this.type = type;
         this.answer = answer;
-        this.other1 = other1;
-        this.other2 = other2;
-        this.other3 = other3;
+        this.other1 = otherAnswers.get(0);
+        this.other2 = otherAnswers.get(1);
+        this.other3 = otherAnswers.get(2);
         this.correct = 0;
     }
 
     public Question() {
 
+    }
+
+
+    @Override
+    public String toString() {
+        if (type == 1) {
+            return "Question: " + question +
+                    "\nTopic: " + topic +
+                    "\nType: Multiple Choice Question" +
+                    "\nAnswer: " + answer +
+                    "\nIncorrect Answer 1: " + other1 +
+                    "\nIncorrect Answer 2: " + other2 +
+                    "\nIncorrect Answer 3: " + other3;
+        } else {
+            return "Question: " + question +
+                    "\nTopic: " + topic +
+                    "\nType: Short Answer Question" +
+                    "\nAnswer: " + answer;
+        }
+    }
+
+    public void save() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(this);
+        session.getTransaction().commit();
     }
 }
