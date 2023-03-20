c69999073bcba713f3b5b251988eb63dc56380b2
nasimhannan <N.Hannan2@ncl.ac.uk>
2023-03-20 15:03:08 +0000

    Added title column to Quiz table

--- src/main/java/csc1035/project2/Quiz.java
+++ src/main/java/csc1035/project2/Quiz.java
@@ -16,6 +16,9 @@ public class Quiz {
     @Column(name = "id")
     private int id; // primary key
 
+    @Column(name = "title")
+    private String title;
+
     @Column(name = "score")
     private int score; // Users previous score of set quiz
 
@@ -100,120 +103,6 @@ public class Quiz {
                 System.out.println("Illegal Access Exception");
             }
         }
-        this.score = 0;
-    }
-
-    /**
-     * This constructor makes up for a 10-question quiz and assigns each question to its specific id
-     * @param qid_1
-     * @param qid_2
-     * @param qid_3
-     * @param qid_4
-     * @param qid_5
-     * @param qid_6
-     * @param qid_7
-     * @param qid_8
-     * @param qid_9
-     * @param qid_10
-     */
-    // 10 Question constructor
-    public Quiz(int qid_1, int qid_2, int qid_3, int qid_4, int qid_5, int qid_6, int qid_7, int qid_8, int qid_9, int qid_10) {
-        this.qid_1 = qid_1;
-        this.qid_2 = qid_2;
-        this.qid_3 = qid_3;
-        this.qid_4 = qid_4;
-        this.qid_5 = qid_5;
-        this.qid_6 = qid_6;
-        this.qid_7 = qid_7;
-        this.qid_8 = qid_8;
-        this.qid_9 = qid_9;
-        this.qid_10 = qid_10;
-        this.score = 0;
-    }
-
-    /**
-     * This constructor makes up for a 15-question quiz and assigns each question to its specific id
-     * @param qid_1
-     * @param qid_2
-     * @param qid_3
-     * @param qid_4
-     * @param qid_5
-     * @param qid_6
-     * @param qid_7
-     * @param qid_8
-     * @param qid_9
-     * @param qid_10
-     * @param qid_11
-     * @param qid_12
-     * @param qid_13
-     * @param qid_14
-     * @param qid_15
-     */
-    // 15 Question constructor
-    public Quiz(int qid_1, int qid_2, int qid_3, int qid_4, int qid_5, int qid_6, int qid_7, int qid_8, int qid_9, int qid_10, int qid_11, int qid_12, int qid_13, int qid_14, int qid_15) {
-        this.qid_1 = qid_1;
-        this.qid_2 = qid_2;
-        this.qid_3 = qid_3;
-        this.qid_4 = qid_4;
-        this.qid_5 = qid_5;
-        this.qid_6 = qid_6;
-        this.qid_7 = qid_7;
-        this.qid_8 = qid_8;
-        this.qid_9 = qid_9;
-        this.qid_10 = qid_10;
-        this.qid_11 = qid_11;
-        this.qid_12 = qid_12;
-        this.qid_13 = qid_13;
-        this.qid_14 = qid_14;
-        this.qid_15 = qid_15;
-        this.score = 0;
-    }
-
-    /**
-     * This constructor makes up for a 20-question quiz and assigns each question to its specific id
-     * @param qid_1
-     * @param qid_2
-     * @param qid_3
-     * @param qid_4
-     * @param qid_5
-     * @param qid_6
-     * @param qid_7
-     * @param qid_8
-     * @param qid_9
-     * @param qid_10
-     * @param qid_11
-     * @param qid_12
-     * @param qid_13
-     * @param qid_14
-     * @param qid_15
-     * @param qid_16
-     * @param qid_17
-     * @param qid_18
-     * @param qid_19
-     * @param qid_20
-     */
-    // 20 Question constructor
-    public Quiz(int qid_1, int qid_2, int qid_3, int qid_4, int qid_5, int qid_6, int qid_7, int qid_8, int qid_9, int qid_10, int qid_11, int qid_12, int qid_13, int qid_14, int qid_15, int qid_16, int qid_17, int qid_18, int qid_19, int qid_20) {
-        this.qid_1 = qid_1;
-        this.qid_2 = qid_2;
-        this.qid_3 = qid_3;
-        this.qid_4 = qid_4;
-        this.qid_5 = qid_5;
-        this.qid_6 = qid_6;
-        this.qid_7 = qid_7;
-        this.qid_8 = qid_8;
-        this.qid_9 = qid_9;
-        this.qid_10 = qid_10;
-        this.qid_11 = qid_11;
-        this.qid_12 = qid_12;
-        this.qid_13 = qid_13;
-        this.qid_14 = qid_14;
-        this.qid_15 = qid_15;
-        this.qid_16 = qid_16;
-        this.qid_17 = qid_17;
-        this.qid_18 = qid_18;
-        this.qid_19 = qid_19;
-        this.qid_20 = qid_20;
         this.score = 0;
     }
 
